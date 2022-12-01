4e257d8db07e8d83d6911d8e1660373d7fdbfb9d
kneelian <3547240@gmail.com>
2022-12-01 13:40:52 +0100

    did a few solutions, settled on the dumbest one
    i had a solution with std::set<int> that was significantly (~15%) slower
    so I chose to do this

--- days/day1.hpp
+++ days/day1.hpp
@@ -1,14 +1,18 @@
 #pragma once
 #include <string>
+#include <sstream>
 #include <utility>
 #include <functional>
+#include <set>
 #include <vector>
 #include <chrono>
+#include "useful.hxx"
 
 	std::ifstream in_values;
 	std::ofstream results;
 
 uint64_t day1part1(bool testing) { 
+	/* boilerplate */
 	if(testing)
 		{ in_values.open("./in/d1-t-in.txt", std::ifstream::in); }
 	else{ in_values.open("./in/d1-in.txt",   std::ifstream::in); }
@@ -26,35 +30,37 @@ uint64_t day1part1(bool testing) {
 	 	std::cerr << "Can't open output file for day 1 pt. 1!\n";
 		return -1;
 	}
-
-	std::string temporary;
-	std::vector<std::string> input;
-
-    while (getline(in_values, temporary))
-    	{ input.push_back(temporary); }
-    in_values.close();
-
-	std::vector<int> input_parsed;
-	int temp = 0;
-
-	for(int i = 0; i < input.size(); i++)
-	{ input_parsed.push_back(std::stoi(input[i],0,10)); }
+	/* boilerplate end */
 
 	auto time_start = std::chrono::high_resolution_clock::now();
 
-		for(int i = 1; i < input_parsed.size(); i++)
-		{ if(input_parsed[i] > input_parsed[i-1]) temp++; }
+    char buffer[8];
+    int res = 0;
+
+    while (!in_values.eof())
+    {
+        int current = 0;
+        while (in_values.getline(buffer, sizeof(buffer)), in_values.gcount() > 1)
+            current += atoi(buffer);
+        res = (current > res)? current: res;
+    }
+
+	results << res; 
 
 	uint64_t time_total = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
-	results << temp;
+	
+	in_values.close();
+	results.close();
 	return time_total;
+	// return
 }
 
 uint64_t day1part2(bool testing) {
-		if(testing)
+	/* boilerplate */
+	if(testing)
 		{ in_values.open("./in/d1-t-in.txt", std::ifstream::in); }
 	else{ in_values.open("./in/d1-in.txt",   std::ifstream::in); }
-
+	
 	if(!in_values.is_open()) { 
 		std::cerr << "Can't open input file for day 1!\n";
 		return -1;
@@ -63,39 +69,56 @@ uint64_t day1part2(bool testing) {
 	if(testing)
 		{ results.open("./out/d1p2-t-out.txt", std::ifstream::in); }
 	else{ results.open("./out/d1p2-out.txt",   std::ifstream::in); }
-	
+
 	if(!results.is_open()) {
 	 	std::cerr << "Can't open output file for day 1 pt. 2!\n";
 		return -1;
 	}
-
-	std::string temporary;
-	std::vector<std::string> input;
-
-    while (getline(in_values, temporary))
-    	{ input.push_back(temporary); }
-    in_values.close();
-
-	std::vector<int> input_parsed;
-
-	for(int i = 0; i < input.size(); i++)
-	{ input_parsed.push_back(std::stoi(input[i],0,10)); }
+	/* boilerplate end */
 
 	auto time_start = std::chrono::high_resolution_clock::now();
-		uint64_t prev_window    = -1;
-		uint64_t current_window =  0;
-		int temp;
 
-		for(int i = 2; i < input_parsed.size(); i++)
-		{
-			if (current_window > prev_window) { temp++; }
-			prev_window = current_window;
-			current_window = input_parsed[i] + input_parsed[i-1] + input_parsed[i-2];
-		}
+    char buffer[8];
+    int top1 = 0;
+    int top2 = 0;
+    int top3 = 0;
 
-	uint64_t time_total = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
-	results << temp;
+    while (!in_values.eof())
+    {
+        int j = 0;
+        while (in_values.getline(buffer, sizeof(buffer)), in_values.gcount() > 1)
+        {
+        	j += atoi(buffer);
+        }
+
+        if(j > top1)
+    	{
+    		top3 = top2;
+    		top2 = top1; 
+    		top1 = j;
+    	} else
+    	if (j > top2)
+    	{
+    		top3 = top2;
+    		top2 = j;
+    	} else
+    	if (j > top3)
+    	{
+    		top3 = j; 
+    	}
+   	}
+
+	results << top1 + top2 + top3; 
+
+	uint64_t time_total = std::chrono::duration_cast
+						 	<std::chrono::nanoseconds>
+						 	(std::chrono::high_resolution_clock::now() - time_start)
+						 .count();
+	
+	in_values.close();
+	results.close();
 	return time_total;
+	// return
 
 }
 
--- days/day2.hpp
+++ days/day2.hpp
@@ -1,23 +1,11 @@
 #pragma once
 #include <string>
+#include <sstream>
 #include <utility>
 #include <functional>
 #include <vector>
 
-template<typename T> std::vector<T> split(const T & str, const T & delimiters) {
-    std::vector<T> v;
-    typename T::size_type start = 0;
-    auto pos = str.find_first_of(delimiters, start);
-    while(pos != T::npos) {
-        if(pos != start) // ignore empty tokens
-            v.emplace_back(str, start, pos - start);
-        start = pos + 1;
-        pos = str.find_first_of(delimiters, start);
-    }
-    if(start < str.length()) // ignore trailing delimiter
-        v.emplace_back(str, start, str.length() - start); // add what's left of the string
-    return v;
-}
+#include "useful.hxx"
 
 uint64_t day2part1(bool testing)
 {
